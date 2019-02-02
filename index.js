17c4da5e2b91d03a0c1482eed3395f5dce171b27
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-02 22:14:27 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function takeANumber(x, name){
+  x++
+  return "Welcome, ${name}. You are number ${x} in line."
+}
+
+function nowServing(n){
+  
+}
+
+function currentLine(n){
+  "The line is currently: 1. Ada, 2. Grace"
+}
